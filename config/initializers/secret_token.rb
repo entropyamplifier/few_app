98e1df7bfeb8f11b7068db61b164790499ab8ec7
Connstance <connstance@gmail.com>
2012-04-28 18:52:00 +0400

    init comm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FewApp::Application.config.secret_token = 'fe4daa674136bba46bf1e7dcc49b0d20d03f67628e24030efe6f88a36ad4cfc647cbfbae8b2920b748aedfcb0305605aad4014254f7fbcc1f8c4071d3a9cdabe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FewApp::Application.config.session_store :cookie_store, key: '_few_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FewApp::Application.config.session_store :active_record_store
