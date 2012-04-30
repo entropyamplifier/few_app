7e74c3c415500556e275ff387687d0872bfab23f
connstance <connstance@gmail.com>
2012-04-30 20:21:24 +0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 FewApp::Application.routes.draw do
-#  get "pages/home"
+  get "users/new"
 
- # get "pages/contact"
+  get "pages/home"
 
-  #get "pages/about"
+  get "pages/contact"
+
+  get "pages/about"
   
-  #get "pages/help"
+  get "pages/help"
+  
+  match '/signup',  :to => 'users#new'
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
